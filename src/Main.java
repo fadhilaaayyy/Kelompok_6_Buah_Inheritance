cdb3e2c4642bcf90970a6ba1d231864428c0a308
ichsan <danilmaulidanil@gmail.com>
2024-10-14 00:17:42 +0700

    Inheritance Buah  Apel

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,22 @@ public class Main {
         System.out.println("Rasa              : " + fr.getRasa());
         System.out.println("Ukuran            : " + fr.getUkuran());
         System.out.println("Simbolis          : " + fr.getSimbolis());
+        
+        Apel ap = new Apel();
+
+        ap.setNama("Apel");
+        ap.setWarna("merah");
+        ap.setBentuk("bulat");
+        ap.setRasa("manis");
+        ap.setUkuran("sedang segenggam tangan");
+        ap.setTekstur_kulit("halus dan bisa dimakan");
+
+        System.out.println("\n\t\t\tKelengkeng");
+        System.out.println("Nama              : " + ap.getNama());
+        System.out.println("Warna             : " + ap.getWarna());
+        System.out.println("Bentuk            : " + ap.getBentuk());
+        System.out.println("Rasa              : " + ap.getRasa());
+        System.out.println("Ukuran            : " + ap.getUkuran());
+        System.out.println("halus dan bisa dimakan         : " + ap.getTekstur_kulit());
     }
 }
