b3fe885f4f2614bd03482ad66473ac7a721eab47
ichsan <danilmaulidanil@gmail.com>
2024-10-14 00:17:42 +0700

    Inheritance Buah  Apel

---
+++ src/Apel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NGUWAWOR
+ */
+public class Apel extends Buah{
+    String tekstur_kulit;
+
+    public String getTekstur_kulit() {
+        return tekstur_kulit;
+    }
+
+    public void setTekstur_kulit(String tekstur_kulit) {
+        this.tekstur_kulit = tekstur_kulit;
+    }
+}
