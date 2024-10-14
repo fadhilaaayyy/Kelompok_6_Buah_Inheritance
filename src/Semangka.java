cfc1960461355b1ee7ff8e1411499e59316d6f49
farelwahyu <hanafijokowire@gmail.com>
2024-10-14 20:20:03 +0700

    buah semangka inheritance

---
+++ src/Semangka.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FADHILA
+ */
+public class Semangka extends Buah{
+    String corak_kkulit;
+
+    public String getCorak_kkulit() {
+        return corak_kkulit;
+    }
+
+    public void setCorak_kkulit(String corak_kkulit) {
+        this.corak_kkulit = corak_kkulit;
+    }
+}
