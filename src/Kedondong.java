d2a45bd90ddd6314c26f35bf128366e05c36ffb0
ahmad luqman hakim <ahmad luqman hakim@MSI>
2024-10-14 19:07:50 +0700

    BuahKedodndong inheritance

---
+++ src/Kedondong.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FADHILA
+ */
+public class Kedondong extends Buah{
+    String Bentuk_Biji;
+
+    public String getBentuk_Biji() {
+        return Bentuk_Biji;
+    }
+
+    public void setBentuk_Biji(String Bentuk_Biji) {
+        this.Bentuk_Biji = Bentuk_Biji;
+    }
+}
