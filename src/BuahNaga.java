f7231d771db0b3310e6ed3ea6d9bb65c216f8498
rinanurrizqi <rinanurrizqi@gmail.com>
2024-10-14 16:17:08 +0700

    tambah buah naga inheritance 2

--- src/BuahNaga.java
+++ src/BuahNaga.java
@@ -3,10 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author Rina
- */
+
 public class BuahNaga extends Buah{
     String bentuk_kulit;
 
