ebdc20ff7b955bd794497dcb604ee7ee0190c9e1
irfanszzz <MyBook Hype@irfan5501>
2024-10-14 14:49:14 +0700

    buah pisang inheritance

---
+++ src/Pisang.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ */
+public class Pisang extends Buah{
+    String bentuk_kulit;
+
+    public String getBentuk_kulit() {
+        return bentuk_kulit;
+    }
+
+    public void setBentuk_kulit(String bentuk_kulit) {
+        this.bentuk_kulit = bentuk_kulit;
+    }
+}
