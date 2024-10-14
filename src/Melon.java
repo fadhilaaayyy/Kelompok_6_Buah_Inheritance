2329905bbe2958a31ec42e3ff4a44855c68e6939
bakolsound <roykang242@gmail.com>
2024-10-14 18:28:00 +0700

    BUAH MELON inheritance Signed-off-by:bakolsound <roykang242@gmail.com>

---
+++ src/Melon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FADHILA
+ */
+public class Melon extends Buah{
+   String tekstur_buah;
+
+    public String getTekstur_buah() {
+        return tekstur_buah;
+    }
+
+    public void setTekstur_buah(String tekstur_buah) {
+        this.tekstur_buah = tekstur_buah;
+    }
+}
